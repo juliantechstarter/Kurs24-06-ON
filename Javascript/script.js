17fb7a177f1db2b72ed96b4ba9792d02b64edb97
juliantechstarter <julian.saenger@edu.techstarter.de>
2024-10-11 12:11:09 +0200

    aufgabe commit

---
+++ Javascript/script.js
@@ -0,0 +1,53 @@
+console.log("JavaScript läuft!");
+
+var alter = 25;         // var - kann neu zugewiesen werden, aber ist global oder funktions-spezifisch
+let name = "Julia";     // let - Block-scoped und kann neu zugewiesen werden
+const beruf = "Lehrer"; // const - Konstante, kann nicht neu zugewiesen werden
+
+console.log(alter, name, beruf);
+
+let zahl = 100;               // Number
+let text = "Hallo Welt";       // String
+let wahr = true;               // Boolean
+let obst = ["Apfel", "Banane"]; // Array
+let person = {name: "Tom", alter: 30}; // Object
+
+console.log(zahl, text, wahr, obst, person);
+
+let num1 = 5;
+let num2 = "5";
+
+console.log(num1 == num2);  // true - nur Wert wird verglichen
+console.log(num1 === num2); // false - Wert und Typ werden verglichen
+
+let punktzahl = 85;
+
+if (punktzahl >= 90) {
+  console.log("Ausgezeichnet");
+} else if (punktzahl >= 70) {
+  console.log("Gut gemacht");
+} else {
+  console.log("Mehr Übung erforderlich");
+}
+
+// for-Schleife von 1 bis 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
+  // while-Schleife von 0 bis 5
+  let j = 0;
+  while (j <= 5) {
+    console.log(j);
+    j++;
+  }
+
+  function begruessung(name) {
+    return "Hallo, " + name + "!";
+  }
+  
+  console.log(begruessung("Anna"));
+  console.log(begruessung("Max"));
+
+  
+  
